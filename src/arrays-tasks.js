c4843be7bd9c840c13fe64d88f748ea58fe73827
Reel <ladias231299@gmail.com>
2024-02-10 22:12:23 +0200

    feat: add solution for 32 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -628,8 +628,24 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  return arr
+    .map((item) => numbers.indexOf(item))
+    .sort()
+    .map((item) => numbers[item]);
 }
 
 /**
