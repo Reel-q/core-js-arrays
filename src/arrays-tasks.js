925f1a906fe1aec586db2d55e129fa661d1cbcce
Reel <ladias231299@gmail.com>
2024-02-10 18:10:02 +0200

    feat: add solution for 17 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -324,10 +324,12 @@ function createNDimensionalArray(n, size) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.reduce((acc, curr) => {
+    return acc.concat(Array.isArray(curr) ? flattenArray(curr) : curr);
+  }, []);
 }
-
+console.log(flattenArray([1, [2, [3, 4], 5], 6]));
 /**
  * Projects each element of the specified array to a sequence
  * and flattens the resulting sequences into one array.
