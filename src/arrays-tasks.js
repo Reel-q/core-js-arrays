884efe6ddfcf789e1e99c268c673e9940a07ab66
Reel <ladias231299@gmail.com>
2024-02-10 21:06:36 +0200

    feat: add solution for 25 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -497,8 +497,15 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr = [];
+  numbers.map((item, i) => {
+    if (item % 2 !== 0) {
+      arr.push(i);
+    }
+    return item;
+  });
+  return arr;
 }
 
 /**
